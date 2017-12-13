d6f2555e2bff05dc09fe0391b91f4663ae81477c
technologyhigh <34485025+technologyhigh@users.noreply.github.com>
2017-12-12 21:17:39 -0500

    Another Tweak to course Gen System

--- js/app.js
+++ js/app.js
@@ -83,8 +83,12 @@ function loadReport(){
 		var row = table.tBodies[0].insertRow(tblRowsCounter);
 
 		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
+		var cCode = row.insertCell(0);
+		var cName = row.insertCell(1);
+		var cSem = row.insertCell(2);
+		var cCred = row.insertCell(3);
+		var cLetr = row.insertCell(4);
+		var cPoin = row.insertCell(5);
 
 		// Add some text to the new cells:
 		cell1.innerHTML = "NEW CELL1";
